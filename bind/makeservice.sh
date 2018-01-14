3056a6ddb402bb7dd56bfd649a6be27247ad84be
dansta <danne@kvantmekanik.se>
2018-01-14 20:39:08 +0100

    This one works locally

--- bind/makeservice.sh
+++ bind/makeservice.sh
@@ -3,7 +3,7 @@
 # Run as root in the same directory as the Dockerfile
 
 # Create image
-docker build -t bind:0.0.100 .
+docker build -t bind:0.0.1 .
 
 # Create east-west and multicast network
 docker network create \
@@ -21,12 +21,11 @@ docker service create \
             --mode global \
             --update-delay 60s \
             --update-parallelism 1 \
-            --dns 127.0.0.1
+            --dns 127.0.0.1 \
             --network bind \
-            --add-host="multicast-bind:224.0.0.2" \
             --mount source=bind,target=/var/log/docker/bind/ \
             --mount source=bind,target=/var/docker/bind/ \
             --name "bind" \
             --publish published=53,target=53 \
-            bind:0.0.100
+            bind:0.0.1
 
