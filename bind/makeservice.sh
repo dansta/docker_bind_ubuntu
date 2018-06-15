9a18630ce9d7040513822589e8e813730b2e4142
dansta <danne@kvantmekanik.se>
2018-06-15 20:03:01 +0200

    Internal dns configuration

--- bind/makeservice.sh
+++ bind/makeservice.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
+set -x
 # Run as root in the same directory as the Dockerfile
 
 # Create image
-docker build -t bind:0.0.1 .
-
-# Create east-west and multicast network
-docker network create \
-            --opt encrypted \
-            --subnet 10.0.0.0/24 \
-            --subnet 224.0.0.0/24 \
-            --driver overlay \
-            bind 
-
-# Create volume for bind
 docker volume create bind
 
 # Create service
@@ -22,13 +12,12 @@ docker service create \
             --update-delay 60s \
             --update-parallelism 1 \
             --dns 127.0.0.1 \
-            --network bind \
             --mount source=bind,target=/var/log/docker/bind/ \
             --mount source=bind,target=/var/docker/bind/ \
             --name "bind" \
             --publish published=53,target=53,protocol=udp \
             --publish published=53,target=53,protocol=tcp \
-            bind:0.0.1
+            192.168.10.150:5000/bind
 
 #https://www.squidblacklist.org/downloads/dns-malicious.zone
 #https://www.squidblacklist.org/downloads/dns-ads.zone
