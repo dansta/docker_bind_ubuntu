0fe80e6f73943c3639ef0720845c3c4b999e32d2
dansta <danne@kvantmekanik.se>
2018-06-16 20:21:28 +0200

    Make it a schedule

--- bind/files/wrapper.sh
+++ bind/files/wrapper.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# initial download
 wget -O /etc/named/mal.zone https://www.squidblacklist.org/downloads/dns-malicious.zone
 wget -O /etc/named/ads.zone https://www.squidblacklist.org/downloads/dns-ads.zone
 
+# statup schedule
+mkdir -p /etc/cron.weekly/
+cat /downloadblacklists > /etc/cron.weekly/downloadblacklists
+chmod ugo+x /etc/cron.weekly/downloadblacklists
 
+
+# start the daemon
 /usr/sbin/named -4 -n 4 -U 4 -p BIND_PORT -u BIND_EFFECTIVE_USER -c BIND_CONF -g
